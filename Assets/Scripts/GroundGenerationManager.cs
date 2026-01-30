443ff4f8397a7bc6bea6f414a79a7c517973bf81
Lorenzo <lorenzo.galfano00@gmail.com>
2026-01-30 17:01:46 +0100

    add random obstacle generation, need to fix bug where object still spawn in the wrong lane

--- Assets/Scripts/GroundGenerationManager.cs
+++ Assets/Scripts/GroundGenerationManager.cs
@@ -7,38 +7,63 @@ public class GroundGenerationManager : MonoBehaviour
     [SerializeField] private List<GameObject> groundSegments_Left = new();
     [SerializeField] private List<GameObject> groundSegments_Right = new();
     [SerializeField] private GameObject groundSegmentPrefab;
+    [SerializeField] private PatternPool patternPool;
     [SerializeField] private float segmentLength = 20f;
     [SerializeField] private float offset = -30.0f;
+    [SerializeField] private GameObject obstaclePrefab;
 
+    private SpawnPattern currentSpawnPattern;
+    private int spawnPatternCounter = -1;
     private List<GameObject> groundChunk = new();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
-
-
+       currentSpawnPattern = patternPool.GetRandomPattern();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PopAndPushGround(groundSegments_Middle, offset);
-        PopAndPushGround(groundSegments_Left, offset);
-        PopAndPushGround(groundSegments_Right, offset);
+        if (groundSegments_Left[0].transform.position.z < offset)
+        {
+            spawnPatternCounter++;
+            if(spawnPatternCounter == 5)
+            {
+                spawnPatternCounter = 0;
+                currentSpawnPattern = patternPool.GetRandomPattern();
+            }
+            Debug.Log("Spawn Pattern Counter: " + spawnPatternCounter);
+        }
+        PopAndPushGround(groundSegments_Middle, offset, 1);
+        PopAndPushGround(groundSegments_Left, offset, 0);
+        PopAndPushGround(groundSegments_Right, offset, 2);
     }
 
-    void PopAndPushGround(List<GameObject> groundSegment, float offset)
+    void PopAndPushGround(List<GameObject> groundSegment, float offset, int column)
     {
         if (groundSegment[0].transform.position.z < offset)
         {
-            GameObject first = groundSegment[0];
-            float scale = first.transform.lossyScale.x;
+            GameObject newSegment = groundSegment[0];
+            float scale = newSegment.transform.lossyScale.x;
             groundSegment.RemoveAt(0);
 
-            GameObject last = groundSegment[^1];
+            GameObject lastSegment = groundSegment[^1];
 
-            first.transform.position = last.transform.position + new Vector3(0, 0, 1) * scale;
+            newSegment.transform.position = lastSegment.transform.position + new Vector3(0, 0, 1) * scale;
 
-            groundSegment.Add(first);
+            // Determine if we need to spawn an object on this segment
+            SpawnDataMapping isObject  = currentSpawnPattern.GetSpawnPattern(spawnPatternCounter, column);
+            
+            // spawn obstacle
+            if (isObject.prefab != null)
+            {
+                GameObject anchor = newSegment.transform.GetChild(0).gameObject;
+                GameObject obstacle = Instantiate(isObject.prefab, anchor.transform.position, Quaternion.identity);
+                obstacle.transform.parent = anchor.transform;
+                obstacle.transform.localPosition = isObject.originOffset;
+            }
+            
+            groundSegment.Add(newSegment);
         }
     }
 
--- Assets/Scripts/SpawnPattern.cs
+++ Assets/Scripts/SpawnPattern.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Assets/Prefab/SpawnPatterns")]
 public class SpawnPattern : ScriptableObject
 {
-    public int rows = 5;
-    public int cols = 3;
+    [System.Serializable]
+    public class Pattern {
+        public int rows = 5;
+        public int cols = 3;
 
-    [SerializeField]
-    private int[] data = new int[15]; // 3 * 5
+        [SerializeField]
+        private SpawnDataMapping[] data = new SpawnDataMapping[15]; // 3 * 5
 
-    public int Get(int row, int col)
+        public SpawnDataMapping Get(int row, int col)
+        {
+            return data[row * cols + col];
+        }
+    }
+
+    [SerializeField] public Pattern pattern;
+
+    public SpawnDataMapping GetSpawnPattern(int row, int col)
     {
-        return data[row * cols + col];
+        return pattern.Get(row, col);
     }
 }
