7036e74adc02b7c57050f0324c1270fb0531519c
Lorenzo <lorenzo.galfano00@gmail.com>
2026-01-30 13:19:49 +0100

    player moves left and right

--- Assets/Script/PlayerControllerEndless.cs
+++ Assets/Script/PlayerControllerEndless.cs
@@ -1,16 +1,66 @@
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float laneOffset = 2.5f;
+    [SerializeField] private float laneChangeSpeed = 10f;
+    [SerializeField] private float yOffset = 5f;
+    private int currentLane = 1; // 0 = left, 1 = middle, 2 = right
+    private Vector3 targetPosition;
+    private Vector3 originPoint;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        InputManager.OnPlayerMovement += HandlePlayerInput;
+        targetPosition = transform.position;
+        originPoint = transform.forward;
+    }
+
+    void OnDestroy()
+    {
+        InputManager.OnPlayerMovement -= HandlePlayerInput;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetPosition,
+            laneChangeSpeed * Time.deltaTime
+        );
     }
+
+    private void HandlePlayerInput(Vector2 movementInput)
+    {
+        if (movementInput.x > 0.5f)
+            ChangeLane(1);
+        else if (movementInput.x < -0.5f)
+            ChangeLane(-1);
+    }
+
+    private void ChangeLane(int direction) {
+        int targetLane = Mathf.Clamp(currentLane + direction, 0, 2);
+        // check if current lane is different, i.e. we are not in the borders
+        if (targetLane == currentLane) return;
+
+        currentLane = targetLane;
+        targetPosition = new Vector3(
+            (currentLane - 1) * laneOffset,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.green;
+        Handles.DrawLine(originPoint + new Vector3(0,yOffset,0), originPoint + new Vector3(0, yOffset, 0)  + (Vector3.forward * 40));
+        Handles.DrawLine(originPoint + new Vector3(0, yOffset, 0) - Vector3.left*5, originPoint + new Vector3(0, yOffset, 0) - Vector3.left * 5 + (Vector3.forward * 40));
+        Handles.DrawLine(originPoint + new Vector3(0, yOffset, 0) + Vector3.left*5, originPoint + new Vector3(0, yOffset, 0) + Vector3.left * 5 + (Vector3.forward * 40));
+    }
+
 }
