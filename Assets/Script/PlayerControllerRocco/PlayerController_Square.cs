8057f28c1db028f85294ef51339175443a1a86f3
Rocco-Borne <roccoborne.rb@gmail.com>
2026-01-30 15:31:55 +0100

    Player Controller Square

--- Assets/Script/PlayerControllerRocco/PlayerController_Square.cs
+++ Assets/Script/PlayerControllerRocco/PlayerController_Square.cs
@@ -4,23 +4,40 @@ using UnityEngine.InputSystem;
 public class PlayerController_Square : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float forwardSpeed = 5.0f;
     [SerializeField] private float jumpForce = 7.0f;
+    [SerializeField] private float rotationSpeed = 10.0f;
 
-    //InputManager inputManager;
+    private Vector2 currDir;
 
     Rigidbody _rb;
 
 
     void Start()
     {
+        InputManager.OnPlayerMovement += HandlePlayerInput;
         _rb = GetComponent<Rigidbody>();
     }
 
  
     void Update()
     {
-        
+
+        _rb.linearVelocity = transform.forward * forwardSpeed;
+        _rb.transform.Rotate(0, currDir.x * speed, 0);
+
+        //_rb.linearVelocity = new Vector3(currDir.x * speed, 0 , forwardSpeed);
+        //_rb.transform.forward = new Vector3(currDir.x * speed, 0, forwardSpeed).normalized;
+
+    }
+    private void OnDestroy()
+    {
+        InputManager.OnPlayerMovement -= HandlePlayerInput;
     }
 
+    private void HandlePlayerInput(Vector2 dir)
+    {
+        currDir = dir;
+    }
 
 }
